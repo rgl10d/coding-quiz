0d76160822f235aca28d82f702880dc15854183f
Garrett Lee <rgl10d@gmail.com>
2020-09-29 16:50:23 -0400

    updated start button and created first question layout

--- script.js
+++ script.js
@@ -2,13 +2,34 @@ console.log("Hello world.");
 
 var quizLanding = document.getElementById("quiz-landing");
 var startButton = document.getElementById("start-quiz");
+var questions = document.getElementById("questions");
+
 
 startButton.addEventListener("click", function(){
     quizLanding.style.display = "none";
-    console.log("hello?")
+    questionOne();
 })
 
-/**
- * On start click, hide landing page and create next question
- * 
- */
+
+// function timer(){
+
+// }
+
+function questionOne(){
+    var oneAnswers = ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"];
+    var firstQuestion = document.createElement("h1");
+
+    firstQuestion.textContent = "Commonly used data types DO NOT include: ";
+    questions.append(firstQuestion);
+
+    for(var i = 0; i < oneAnswers.length; i++){
+       var buttons = document.createElement("button");
+       var lineBreak = document.createElement("br");
+        buttons.textContent = oneAnswers[i];
+        buttons.setAttribute("class", "btn btn-primary");
+        buttons.setAttribute("data-value", oneAnswers[i]);
+        buttons.style.marginTop = "10px";
+        questions.append(buttons);
+        questions.append(lineBreak);
+    }
+}
